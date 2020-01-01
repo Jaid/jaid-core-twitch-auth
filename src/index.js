f38d07cf72033a9378bbb9cae0202eca244d08fb
Jaid <jaid.jsx@gmail.com>
2020-01-01 03:11:29 +0100

    Added stubs
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,130 @@
 /** @module jaid-core-twitch-auth */
 
+import Router from "@koa/router"
+import {KoaPassport} from "koa-passport"
+import {Strategy as TwitchStrategy} from "passport-twitch-new"
+import {isString} from "lodash"
+import koaBodyparser from "koa-bodyparser"
+import TwitchUser from "src/models/TwitchUser"
+
+import indexTemplate from "./auth.hbs"
+
 /**
- * @function
- * @param {number} [compareValue]
- * @returns {number} Seconds passed since Unix epoch (01 January 1970)
- * @example
- * import jaidCoreTwitchAuth from "jaid-core-twitch-auth"
- * const result = jaidCoreTwitchAuth()
- * result === 1549410770
- * setTimeout(() => {
- *   const result2 = jaidCoreTwitchAuth(result)
- *   result2 === 3
- * }, 3000)
+ * @typedef Options
+ * @prop {string|string[]} scope
+ * @prop {string} domain
  */
-export default compareValue => {
-  const seconds = Math.floor(Date.now() / 1000)
-  if (compareValue === undefined) {
-    return seconds
-  } else {
-    return seconds - compareValue
+
+export default class TwitchAuthPlugin {
+
+  /**
+   * @type {string}
+   */
+  clientId = null
+
+  /**
+   * @type {string}
+   */
+  clientSecret = null
+
+  /**
+   * @type {string}
+   */
+  callbackUrl = null
+
+  /**
+   * @constructor
+   * @param {Options} options
+   */
+  constructor(options = {}) {
+    this.options = {
+      scope: [],
+      domain: null,
+      ...options,
+    }
+    if (isString(this.options.scope)) {
+      this.options.scope = [this.options.scope]
+    }
   }
+
+  /**
+   * @return {import("essential-config").Options}
+   */
+  getConfigSetup = () => {
+    return {
+      secretKeys: ["twitchClientSecret"],
+      defaults: {
+        twitchClientId: "ENTER",
+        twitchClientCallbackUrl: this.options.domain ? `https://${this.options.domain}/auth/twitch/callback` : "ENTER",
+      },
+    }
+  }
+
+  setCoreReference(core) {
+    /**
+     * @type {import("jaid-core").default}
+     */
+    this.core = core
+  }
+
+  handleConfig(config) {
+    this.clientId = config.twitchClientId
+    this.clientSecret = config.twitchClientSecret
+    this.callbackUrl = config.twitchClientCallbackUrl
+  }
+
+  /**
+   * @param {string} accessToken
+   * @param {string} refreshToken
+   * @param {Object} profile
+   * @param {Function} done
+   */
+  async verify(accessToken, refreshToken, profile, done) {
+    await TwitchUser.upsert({
+      accessToken,
+      refreshToken,
+      broadcasterType: profile.broadcaster_type,
+      description: profile.description,
+      displayName: profile.display_name,
+      twitchId: profile.id,
+      loginName: profile.login,
+      offlineImageUrl: profile.offline_image_url,
+      avatarUrl: profile.profile_image_url,
+      viewCount: profile.view_count,
+    })
+    console.log("Login from Twitch user %s", profile.login)
+    done()
+  }
+
+  // collectModels() {
+  //   return {
+  //     TwitchUser,
+  //   }
+  // }
+
+  /**
+   * @param {import("koa")} koa
+   */
+  handleKoa(koa) {
+    const bodyparserMiddleware = koaBodyparser()
+    this.passport = new KoaPassport
+    const strategy = new TwitchStrategy({
+      scope: this.options.scope,
+      clientID: this.clientId,
+      clientSecret: this.clientSecret,
+      callbackURL: this.callbackUrl,
+    }, this.verify.bind(this))
+    this.passport.use(strategy)
+    this.router = new Router
+    this.router.get("/auth", bodyparserMiddleware, context => {
+      context.type = "html"
+      context.body = indexTemplate()
+    })
+    this.router.get("/auth/twitch", bodyparserMiddleware, this.passport.authenticate("twitch"))
+    this.router.get("/auth/twitch/callback", bodyparserMiddleware, this.passport.authenticate("twitch", {failureRedirect: "/"}), context => {
+      context.body = "OK"
+    })
+    koa.use(this.router.routes())
+  }
+
 }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,56 @@
 import path from "path"
 
+import fsp from "@absolunet/fsp"
+import delay from "delay"
+import getPort from "get-port"
+import JaidCore from "jaid-core"
+import ms from "ms.macro"
+
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
 /**
  * @type { import("../src") }
  */
-const {default: jaidCoreTwitchAuth} = indexModule
+const {default: Plugin} = indexModule
 
-it("should run", () => {
-  const result = jaidCoreTwitchAuth()
-  expect(result).toBeGreaterThan(1549410770)
-})
+it("should run", async () => {
+  let authHomepageResponse
+  const insecurePort = await getPort()
+  const twitchAuthPlugin = new Plugin()
+  const core = new JaidCore({
+    insecurePort,
+    name: _PKG_TITLE,
+    folder: ["Jaid", _PKG_TITLE, "test", new Date().toISOString()],
+    version: _PKG_VERSION,
+    useGot: true,
+    sqlite: true,
+  })
+  const secretsFile = path.join(core.appFolder, "secrets.yml")
+  await fsp.writeYaml(secretsFile, {
+  })
+  const testClientClass = class {
+
+    init() {
+      /**
+       * @type {import("got").Got}
+       */
+      this.got = core.got.extend({
+        prefixUrl: "http://localhost",
+        port: insecurePort,
+      })
+    }
+
+    async ready() {
+      core.logger.info(`Link: http://localhost:${insecurePort}/auth`)
+      authHomepageResponse = await this.got("auth")
+    }
+
+  }
+  await core.init({
+    dashboard: twitchAuthPlugin,
+    test: testClientClass,
+  })
+  await delay(ms`30 seconds`)
+  expect(authHomepageResponse.statusCode).toBe(200)
+  await core.close()
+}, ms`40 seconds`)
